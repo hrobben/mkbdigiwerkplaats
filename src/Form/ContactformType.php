f920d1c28cbe31b90e8afa4cd38cdce0f7f6ece8
Zt-freak <bnkho98@hotmail.com>
2019-11-22 11:53:31 +0100

    changed CSS

--- src/Form/ContactformType.php
+++ src/Form/ContactformType.php
@@ -7,14 +7,44 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 class ContactformType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name', null, ['attr' => [ 'class' => 'inputfield--contact', 'placeholder' => 'Naam' ], 'label' => false])
-            ->add('email', null, ['attr' => [ 'class' => 'inputfield--contact', 'placeholder' => 'E-mail' ], 'label' => false])
-            ->add('message', null, ['attr' => [ 'class' => 'inputfield--contact', 'placeholder' => 'Uw Bericht' ], 'label' => false])
+            ->add(
+                'name', null,
+                [
+                    'attr' => [
+                        'class' => 'inputfield--contact',
+                        'placeholder' => 'Naam' 
+                    ],
+                    'label' => false
+                ]
+            )
+            ->add(
+                'email', null,
+                [
+                    'attr' => [
+                        'class' => 'inputfield--contact',
+                        'placeholder' => 'E-mail'
+                    ],
+                    'label' => false
+                ]
+            )
+            ->add(
+                'message', TextareaType::class,
+                [
+                    'attr' => [
+                        'rows' => 5,
+                        'class' => 'inputfield--contact',
+                        'placeholder' => 'Uw Bericht',
+                    ],
+                    'label' => false
+                ]
+            )
         ;
     }
 
